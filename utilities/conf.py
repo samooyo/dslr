c67a04be295314a2650b92511ca301cda5368bd3
sadarnau <samidarnaud@gmail.com>
2021-11-15 14:51:14 +0100

    small fix in typo

--- utilities/conf.py
+++ utilities/conf.py
@@ -1,3 +1,3 @@
-SELECTED_COURSES = ['Divination', 'Transfiguration', 'Astronomy', 'Charms']
+SELECTED_COURSES = ['Divination', 'Transfiguration', 'Astronomy', 'Charms', 'Flying']
 HOUSES = ['Ravenclaw', 'Slytherin', 'Gryffindor', 'Hufflepuff']
-SELECTED_COURSES_AND_HOUSES = ['Hogwarts House', 'Divination', 'Transfiguration', 'Astronomy', 'Charms']
+SELECTED_COURSES_AND_HOUSES = ['Hogwarts House', 'Divination', 'Transfiguration', 'Astronomy', 'Charms', 'Flying']
